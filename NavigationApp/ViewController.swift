657e5a0f4cd36696ac1d8753a8c4f84584a2b4ae
DanyaDankou <93947618+DanyaDankou@users.noreply.github.com>
2022-11-13 21:38:17 +0300

    Initial commit

---
+++ NavigationApp/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  NavigationApp
+//
+//  Created by comp on 13.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let storyboardInstance = UIStoryboard(name: "Main", bundle: nil)
+    
+    @IBAction func toGreenScene(_ sender: UIButton) {
+        let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "greenViewController")
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
+
+    
+    @IBAction func toYellowScene(_ sender: UIButton) {
+        let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "yellowViewController")
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
+    
+    @IBAction func toRootScene(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
